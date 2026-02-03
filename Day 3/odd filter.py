c13475c597d98091c9d0807b172f718707169e1e
Shyadha Fathima <165204122+ShyadhaJamimullah@users.noreply.github.com>
2026-02-03 09:33:57 +0530

    Delete Day 3 directory

--- Day 3/odd filter.py
+++
@@ -1,5 +0,0 @@
-l1=[int(x) for x in input("Enter the numbers with space:").split()]
-even=list(filter(lambda x:x%2==0,l1))
-odd=list(filter(lambda x:x%2!=0,l1))
-print("Even numbers:",even)
-print("Odd numbers",odd)
