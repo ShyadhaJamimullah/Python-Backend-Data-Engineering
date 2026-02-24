9857724eddd68f87eab3ddd68a82a262626c2695
Shyadha Fathima <165204122+ShyadhaJamimullah@users.noreply.github.com>
2026-02-24 11:35:34 +0530

    Update auth.py

--- FastAPI + Backend + ETL APIs/HR Analytics API with Authentication, API Versioning and Logging/auth.py
+++ FastAPI + Backend + ETL APIs/HR Analytics API with Authentication, API Versioning and Logging/auth.py
@@ -1,32 +1,41 @@
-from fastapi import Depends,HTTPException,status
+from fastapi import Depends, HTTPException,status
 from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
 from jose import JWTError,jwt
 import os
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+
+pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+def hashed_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password:str,hash_password:str)->bool:
+    return pwd_context.verify(plain_password, hash_password)
+
 
 
 security=HTTPBearer()
 
-SECRET_KEY=os.getenv("jwt_secret_key","supersecretkey")
+SECRET_KEY=os.getenv("jwt_secret_key", "supersecretkey")
 ALGORITHM="HS256"
 JWT_EXPIRE_MINUTES=30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire=datetime.utcnow() + timedelta(minutes=JWT_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=JWT_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_jwt(
-    credentials: HTTPAuthorizationCredentials=Depends(security)
+    credentials: HTTPAuthorizationCredentials= Depends(security)
 ):
     token=credentials.credentials
 
     try:
-        payload=jwt.decode(token, SECRET_KEY,algorithms=[ALGORITHM])
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
       
         user_id=payload.get("sub")
